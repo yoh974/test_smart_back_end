f9d9fbc76dddc776bff64ee4838104b49ad0cd27
lionel <sornom.lionel@gmail.com>
2020-07-10 21:53:56 +0200

    add confirmation question

--- src/Command/ImportCSV.php
+++ src/Command/ImportCSV.php
@@ -11,6 +11,7 @@ use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class ImportCSV extends Command
 {
@@ -46,54 +47,62 @@ class ImportCSV extends Command
         }
         //if empty csv or file not found
         if (!empty($rows)) {
-            // Turning off doctrine default logs queries for saving memory
-            $this->em->getConnection()->getConfiguration()->setSQLLogger(null);
+            $helper = $this->getHelper('question');
+            $question = new ConfirmationQuestion(
+                "Etes vous sûr de vouloir importer les données? y|n \n",
+                false,
+                '/^(y|j)/i'
+            );
 
-            // Define the size of record, the frequency for persisting the data and the current index of records
-            $size = count($rows);
-            $batchSize = 20;
-            $i = 1;
+            if ($helper->ask($input, $output, $question)) {
+                // Turning off doctrine default logs queries for saving memory
+                $this->em->getConnection()->getConfiguration()->setSQLLogger(null);
 
-            // Starting progress
-            $progress = new ProgressBar($output, $size);
-            $progress->start();
+                // Define the size of record, the frequency for persisting the data and the current index of records
+                $size = count($rows);
+                $batchSize = 20;
+                $i = 1;
+
+                // Starting progress
+                $progress = new ProgressBar($output, $size);
+                $progress->start();
 
 
-            // Processing on each row of data
-            foreach ($rows as $row) {
-                $library = new Library();
+                // Processing on each row of data
+                foreach ($rows as $row) {
+                    $library = new Library();
 
-                //set object
-                $library
-                    ->setTitle($row['Titre'])
-                    ->setName($row['Nom'])
-                    ->setFirstname($row['Prenom'])
-                    ->setEditor($row['Editeur'])
-                    ->setBookFormat($row['Format livre'])
-                    ->setType($row['Type'])
-                    ->setSection($row['Section']);
-                $this->em->persist($library);
+                    //set object
+                    $library
+                        ->setTitle($row['Titre'])
+                        ->setName($row['Nom'])
+                        ->setFirstname($row['Prenom'])
+                        ->setEditor($row['Editeur'])
+                        ->setBookFormat($row['Format livre'])
+                        ->setType($row['Type'])
+                        ->setSection($row['Section']);
+                    $this->em->persist($library);
 
-                // Each 20 users persisted we flush everything
-                if (($i + 1 % $batchSize) === 0) {
-                    $this->em->flush();
-                    // Detaches all objects from Doctrine for memory save
-                    $this->em->clear();
+                    // Each 20 users persisted we flush everything
+                    if (($i + 1 % $batchSize) === 0) {
+                        $this->em->flush();
+                        // Detaches all objects from Doctrine for memory save
+                        $this->em->clear();
 
-                    // Advancing for progress display on console
-                    $progress->advance($batchSize);
-                    $output->writeln(' of datas imported ... ');
+                        // Advancing for progress display on console
+                        $progress->advance($batchSize);
+                        $output->writeln(' of datas imported ... ');
+                    }
+                    $i++;
                 }
-                $i++;
+
+                // Flushing and clear data on queue
+                $this->em->flush();
+                $this->em->clear();
+
+                // Ending the progress bar process
+                $progress->finish();
             }
-
-            // Flushing and clear data on queue
-            $this->em->flush();
-            $this->em->clear();
-
-            // Ending the progress bar process
-            $progress->finish();
-
             return Command::SUCCESS;
         } else {
             $output->writeln("Erreur CSV vide");
